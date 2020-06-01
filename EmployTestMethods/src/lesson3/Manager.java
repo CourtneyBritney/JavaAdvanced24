06220440c2c0e460a119a8b1ed9fa56a1d1ad854
CourtneyBritney <courtney02alexander@gmail.com>
2020-06-01 18:22:39 +0200

    My First Commit.

---
+++ EmployTestMethods/src/lesson3/Manager.java
@@ -0,0 +1,24 @@
+package lesson3;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+
+    public Manager(String deptName, int empId, String name, String ssn, double salary) {
+        super(empId, name, ssn, salary);
+        this.deptName = deptName;
+    }
+
+
+}
+
+
